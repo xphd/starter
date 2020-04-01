d0d7bddc3abfab3750cd907190e17a387c9f5a27
alex <cong.cs.tufts@gmail.com>
2020-04-01 14:47:56 -0400

    add boostrap

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -8,6 +8,15 @@ const socket = new VueSocketIO({
 });
 Vue.use(socket);
 
+// use bootstrap
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+// Install BootstrapVue
+Vue.use(BootstrapVue);
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin);
+
 Vue.config.productionTip = false;
 
 new Vue({
