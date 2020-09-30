73d9eb52ece8fc72233a887ee30856ef58a418d4
alex <>
2020-09-29 21:57:59 -0400

    update

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,7 +1,10 @@
 // vue.config.js
 module.exports = {
   devServer: {
-    publicPath: "/",
+    // public: "0.0.0.0:8080",
+    // disableHostCheck: true,
+    // proxy: "http://localhost:9090/",
+    // publicPath: "/",
     proxy: {
       "/socket.io": {
         target: "http://localhost:9090/",
