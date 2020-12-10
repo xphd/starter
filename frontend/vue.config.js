27b60cb7d854b74e43205926e8cfe349d5c228f7
alex <>
2020-12-10 18:24:06 -0500

    avoid localhost/socketjs-node info ERR_CERT_AUTHORITY_INVALID

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,6 +1,9 @@
 // vue.config.js
 module.exports = {
   devServer: {
+    host: "0.0.0.0", // have this to avoid localhost/socketjs-node info ERR_CERT_AUTHORITY_INVALID
+    // https: false,
+    // hot: true,
     // disableHostCheck: true,
     proxy: {
       "/socket.io": {
